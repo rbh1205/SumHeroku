1e37c1e24aa2966b13032f5639c223bea130fb76
kyseler <ulrik-ulle@hotmail.com>
2020-11-10 09:29:46 +0100

    test

---
+++ test/Integrationstest.js
@@ -0,0 +1,8 @@
+require('should');
+const request = require('supertest');
+const controller = require("../controllers/controller");
+const app = require('../app.js');
+const { response } = require ('..app.js');
+
+
+
