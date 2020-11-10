bfabe70c675ccd1841f65583a5dbca690ed7bd1a
kyseler <ulrik-ulle@hotmail.com>
2020-11-10 09:48:49 +0100

    IntegrationsTestProduct

--- test/Integrationstest.js
+++
@@ -1,8 +0,0 @@
-require('should');
-const request = require('supertest');
-const controller = require("../controllers/controller");
-const app = require('../app.js');
-const { response } = require ('..app.js');
-
-
-
